89052eb3e2d02a050ff689e7e9599675d54d4096
CrisEspinoza <cristian.espinoza.s@usach.cl>
2018-04-14 01:31:43 -0300

    Codigo de bot

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=mermelab-Web-Deploy.pubxml -p:Password=PiXfc5gNtPLwePdPP7DEEiji2c06uuxaKNLMlial7joGHa1XimJr05YRAi28
+
